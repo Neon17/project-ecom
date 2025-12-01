4c457c2bb03c0d910b5695527c6ac335964d5db6
Neon17 <neon.neupane.9@gmail.com>
2025-12-01 13:44:43 +0545

    feat: rich text editor added and fixed

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -162,7 +162,7 @@
     </style>
 
     <!-- TinyMCE -->
-    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+    <script src="https://cdn.tiny.cloud/1/{{ config('services.tinymce.key', 'no-api-key') }}/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
     <script>
         tinymce.init({
             selector: '#description',
